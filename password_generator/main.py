ad58be2ddd9676ce621c4a04bbb84b7100a6e6d4
jjunno <joonas.junno@protonmail.com>
2025-09-21 10:00:48 +0300

    use of argv

--- password_generator/main.py
+++ password_generator/main.py
@@ -1,9 +1,14 @@
+import sys
 from password import Password
 
 def main():
-    testInput = 'korhonen'
-    pw = Password(testInput)
-    pw.read_files()
+    use_password = None
+    if len(sys.argv) is 2:
+        use_password = sys.argv[1]
+    else:
+        use_password = 'sieni'
+    pw = Password(use_password)
+    pw.generate()
 
 if __name__ == "__main__":
     main() 
--- password_generator/password.py
+++ password_generator/password.py
@@ -4,6 +4,9 @@ class Password:
         self.filenames = ["firstnames_men_fi_avoindata.json", "firstnames_women_fi_avoindata.json", "surnames_fi_avoindata.json", "lang_fi_kotus.json"]
         self.words = []
 
+        print(f"Original password is \"{original}\"")
+        self.read_files()
+
     def read_files(self):
         for filename in self.filenames:
                 file = open(f"wordlists_json/{filename}", "r", encoding="utf-8")
@@ -12,3 +15,6 @@ class Password:
                     self.words.append(line)
                 
         print(f"{len(self.filenames)} files read, total {len(self.words)} words found")
+
+    def generate(self):
+        print(f"debug")
